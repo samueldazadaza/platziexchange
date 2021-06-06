51423a2d91cd3b5d9e597208d21f1471008548b1
samuel dazad <samueldazadaza@gmail.com>
2021-06-06 18:05:48 -0500

    Mejorar la UI con filtros31/38

--- src/components/PxAssetsTable.vue
+++ src/components/PxAssetsTable.vue
@@ -19,14 +19,22 @@
         :key="a.id"
         class="border-b border-gray-200 hover:bg-gray-100 hover:bg-orange-100"
       >
-        <td><img :src="`https://static.coincap.io/assets/icons/${a.symbol.toLowerCase()}@2x.png`" :alt="a.name"></td>
+        <td>
+          <img
+            class="w-6 h-6"
+            :src="`https://static.coincap.io/assets/icons/${a.symbol.toLowerCase()}@2x.png`" 
+            :alt="a.name"
+          />
+        </td>
         <td>
           <b>#{{ a.rank }}</b>
         </td>
         <td> {{ a.name }} </td>
-        <td> {{ a.priceUsd }} </td>
-        <td> {{ a.marketCapUsd }} </td>
-        <td> {{ a.changePercent24hr }} </td>
+        <td> {{ a.priceUsd | dollar }} </td>
+        <td> {{ a.marketCapUsd | dollar }} </td>
+          <td :class="a.changePercent24hr.includes('-') ? 'text-red-600' : 'text-green-600' ">
+            {{ a.changePercent24hr | percent }}
+          </td>
         <td class="hidden sm:block"></td>
       </tr>
     </tbody>
